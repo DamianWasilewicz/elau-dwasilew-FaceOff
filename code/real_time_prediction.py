af4d6e831f081b4dd76de78a8f724a1b1556f18b
Damian Wasilewicz <damian_wasilewicz@brown.edu>
2021-04-19 15:44:55 -0400

    getting predictions to show up on camera, inaccurate

---
+++ code/real_time_prediction.py
@@ -0,0 +1,45 @@
+# import the necessary packages
+from imutils.video import VideoStream
+from imutils.video import FPS
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+
+# construct the argument parse and parse the arguments
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-p", "--prototxt", required=True,
+# 	help="path to Caffe 'deploy' prototxt file")
+# ap.add_argument("-m", "--model", required=True,
+# 	help="path to Caffe pre-trained model")
+# ap.add_argument("-c", "--confidence", type=float, default=0.2,
+# 	help="minimum probability to filter weak detections")
+# args = vars(ap.parse_args())
+
+
+# initialize the video stream, allow the cammera sensor to warmup,
+# and initialize the FPS counter
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+fps = FPS().start()
+
+while True:
+    frame = vs.read()
+    # frame = imutils.resize(frame, width=400)
+    (h,w) = frame.shape[:2]
+    frame = np.array(frame, dtype=np.uint8)
+    print("Getting image")
+    print("Dimensions height", np.shape(frame))
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+	# if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
+    fps.update()
+fps.stop()
+cv2.destroyAllWindows()
+vs.stop()
+
+
--- code/run.py
+++ code/run.py
@@ -2,10 +2,18 @@ from preprocess import getData, getDataWithoutNan, splitUpTestingData, splitUpTr
 from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Dropout, Dense, Flatten
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.callbacks import ModelCheckpoint
+from skimage.color import rgb2gray 
 import numpy as np
 import hyperparameters as hp
 import math
 import matplotlib.pyplot as plt
+from imutils.video import VideoStream
+from imutils.video import FPS
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
 
 
 def main():
@@ -13,22 +21,22 @@ def main():
     trainingData, testingData = getData(
         '../data/training.csv', '../data/test.csv')
 
-    train_key_pts, train_imgs = splitUpTrainData(trainingData)
-    print("First set of train points", train_key_pts[0])
-    print("First set of images", train_imgs[0])
+    # train_key_pts, train_imgs = splitUpTrainData(trainingData)
+    # print("First set of train points", train_key_pts[0])
+    # print("First set of images", train_imgs[0])
 
     test_ids, test_imgs = splitUpTestingData(testingData)
 
-    print("Image dimensions", np.shape(train_imgs))
+    # print("Image dimensions", np.shape(train_imgs))
 
-    print("Number of testing points", train_key_pts.shape[0])
-    train_key_pts, train_imgs = getDataWithoutNan(train_key_pts, train_imgs)
-    print("Number of testing points without nan", len(train_key_pts))
+    # print("Number of testing points", train_key_pts.shape[0])
+    # train_key_pts, train_imgs = getDataWithoutNan(train_key_pts, train_imgs)
+    # print("Number of testing points without nan", len(train_key_pts))
 
-    train_key_pts, train_imgs = mirrorData(train_key_pts, train_imgs)
-    print("Number of testing points with flipping", train_key_pts.shape[0])
+    # train_key_pts, train_imgs = mirrorData(train_key_pts, train_imgs)
+    # print("Number of testing points with flipping", train_key_pts.shape[0])
 
-    train_imgs = np.reshape(train_imgs, (train_imgs.shape[0], train_imgs.shape[1], train_imgs.shape[2], 1))
+    # train_imgs = np.reshape(train_imgs, (train_imgs.shape[0], train_imgs.shape[1], train_imgs.shape[2], 1))
     reshaped_test_imgs = np.reshape(test_imgs, (test_imgs.shape[0], test_imgs.shape[1], test_imgs.shape[2], 1))
 
     FPModel = buildModel()
@@ -40,6 +48,9 @@ def main():
     # callbacks_list = [checkpoint]
     loadWeights(FPModel)
     visualize(test_imgs, reshaped_test_imgs, FPModel)
+
+    real_time_prediction(FPModel)
+
 
 
 
@@ -88,10 +99,11 @@ def test(testingData, model):
         # if math.abs(pred - )
 
 def visualize(testingData, reshaped_test_imgs, model):
-    for image in range(testingData.shape[0]):
+    for image in range(5):
         print("Plotting image")
         plt.imshow(testingData[image])
         reshaped_img = np.reshape(reshaped_test_imgs[image], (1, hp.image_dim, hp.image_dim, 1))
+        print("Reshaped img shape", np.shape(reshaped_img))
         predicted_pts = model.predict(reshaped_img)
         print("Predicted pts shape", np.shape(predicted_pts))
         for pt in range(30):
@@ -99,6 +111,66 @@ def visualize(testingData, reshaped_test_imgs, model):
                 circle=plt.Circle( (predicted_pts[0,pt], predicted_pts[0,pt + 1]), radius=1, color='r')
                 plt.gca().add_patch(circle)
         plt.show()
+    
+def real_time_prediction(model):
+    print("[INFO] starting video stream...")
+    vs = VideoStream(src=0).start()
+    time.sleep(2.0)
+    fps = FPS().start()
+
+    while True:
+        frame = vs.read()
+        # frame = imutils.resize(frame, width=400)
+        (h,w) = frame.shape[:2]
+        frame = np.array(frame, dtype=np.uint8)
+        frame = imutils.resize(frame, width=400)
+        model_frame = cv2.resize(frame, (hp.image_dim, hp.image_dim))
+        gray_image = rgb2gray(model_frame)
+        model_input = np.reshape(gray_image, (1, hp.image_dim, hp.image_dim, 1))
+        # frame = np.reshape(frame, (1, hp.image_dim, hp.image_dim, 1))
+        prd = np.array(model(model_input))
+        print("Predictions length", len(prd))
+        new_image = frame
+        # prd = prd / 96
+       
+        for pt in range(prd.shape[1]):
+            if (pt %2 == 0):
+                prd[0][pt] = (prd[0][pt] / 96) * 400
+                prd[0][pt + 1] = (prd[0][pt + 1] / 96) * 255
+                print("X", prd[0][pt])
+                print("Y", prd[0][pt + 1])
+
+    
+        
+        new_image = cv2.circle(new_image, (int(prd[0][0]), int(prd[0][1])), 2, (0,0,255))
+        new_image = cv2.circle(new_image, (int(prd[0][2]), int(prd[0][3])), 2, (0,0,255))
+        new_image = cv2.circle(new_image, (int(prd[0][4]), int(prd[0][5])), 2, (0,0,255))
+        new_image = cv2.circle(new_image, (int(prd[0][6]), int(prd[0][7])), 2, (0,0,255))
+        new_image = cv2.circle(new_image, (int(prd[0][8]), int(prd[0][9])), 2, (0,0,255))
+        new_image = cv2.circle(new_image, (int(prd[0][10]), int(prd[0][11])), 2, (0,0,255))
+        new_image = cv2.circle(new_image, (int(prd[0][12]), int(prd[0][13])), 2, (0,0,255))
+        new_image = cv2.circle(new_image, (int(prd[0][14]), int(prd[0][15])), 2, (0,0,255))
+        new_image = cv2.circle(new_image, (int(prd[0][16]), int(prd[0][17])), 2, (0,0,255))
+        new_image = cv2.circle(new_image, (int(prd[0][18]), int(prd[0][19])), 2, (0,0,255))
+        new_image = cv2.circle(new_image, (int(prd[0][20]), int(prd[0][21])), 2, (0,0,255))
+        new_image = cv2.circle(new_image, (int(prd[0][22]), int(prd[0][23])), 2, (0,0,255))
+        new_image = cv2.circle(new_image, (int(prd[0][24]), int(prd[0][25])), 2, (0,0,255))
+        new_image = cv2.circle(new_image, (int(prd[0][26]), int(prd[0][27])), 2, (0,0,255))
+        new_image = cv2.circle(new_image, (int(prd[0][28]), int(prd[0][29])), 2, (0,0,255))
+
+        print("Prediction pts shape", np.shape(prd))
+        print("Getting image")
+        print("Dimensions height", np.shape(frame))
+        # circle = cv2.circle(new_image, (48,48), 3, (0, 0, 255))
+        cv2.imshow("Frame", new_image)
+        key = cv2.waitKey(1) & 0xFF
+	    # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            break
+        fps.update()
+    fps.stop()
+    cv2.destroyAllWindows()
+    vs.stop()
 
 
 
